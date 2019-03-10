e183a75ff3635018f21ac541356f04628b53fe32
xsaco07 <xsaco07@gmail.com>
2019-03-09 19:47:15 -0600

    Adapt main program to use the SYSCALLS module based in fork_test.c example

--- makefile
+++ makefile
@@ -1,15 +1,16 @@
 TARGET1 = rastreador
 TARGET2 = prog
+TARGET3 = SysCalls
 
 all : compile
 
 compile :
 
-	gcc $(TARGET1).c -o $(TARGET1)
+	gcc $(TARGET1).c $(TARGET3).c -o $(TARGET1)
 	gcc $(TARGET2).c -o $(TARGET2)
 
 execute_ls:
-	./$(TARGET1) -v ls -ll
+	./$(TARGET1) -v ls -a
 
 execute_prog:
 	./$(TARGET1) -V ./$(TARGET2) -a -b -c
--- rastreador.c
+++ rastreador.c
@@ -4,6 +4,7 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <string.h>
+#include "SysCalls.h"
 
 #define TRACER_OPTION_1 "-V"
 #define TRACER_OPTION_2 "-v"
@@ -11,6 +12,7 @@
 #define TRUE 1
 #define FALSE 0
 #define CHILD_DEFAULT_PID 0
+#define PROG_NAME_POSITION 0
 
 char* PROG;
 char* PROG_ARGS[MAX_ARGS];
@@ -26,14 +28,97 @@ void print_arguments(char* array[]);
 _Bool prog_at_position_has_arguments(int position);
 _Bool V_flag_found();
 void take_prog_arguments_from_argv(char* argv[]);
+void manage_arguments(char* argv[]);
+void manage_V_argument(char* argv[]);
 
 int main(int argc, char *argv[]){
 
 	argument_count = argc;
+
 	initialize_args_array();
 
-	printf("\nThe first argument is: %s\n", argv[1]);
+	manage_arguments(argv);
 
+	initialize_sys_calls_array();
+
+  int proc_status;
+  long sys_call_number;
+  long sys_call_called = 0;
+  struct user_regs_struct regs;
+
+  pid_t child_pid = fork();
+
+  if (child_pid < CHILD_DEFAULT_PID) { // Error in proccess
+    perror("Error in fork()");
+    exit(-1);
+  }
+
+  else if (child_pid == CHILD_DEFAULT_PID) { // Child proccess
+		printf("\nThe program %s will be executed\n", PROG);
+		printf("\nThe arguments of %s are:\n", PROG);
+		print_arguments(PROG_ARGS);
+		printf("-------PROG EXECUTION--------\n\n");
+    ptrace(PTRACE_TRACEME, CHILD_DEFAULT_PID, NULL, NULL);
+    execvp(PROG, PROG_ARGS);
+  }
+  else{ // Parent proccess
+    while (1) {
+      waitpid(-1, &proc_status, 0); // Wait for the kernel to tell us that the child process state has
+                                    // changed or been interrupted
+
+      if(WIFEXITED(proc_status)){ // Check wether the process has finished or exit
+        break;
+      }
+
+      if (sys_call_called == FALSE) {
+        sys_call_called = TRUE;
+        ptrace(PTRACE_GETREGS, child_pid, NULL, &regs); // Get access to the registers where sys calls info is
+        sys_call_number = regs.orig_rax;
+
+				if (TRACER_FLAG_1) {
+					print_sys_call_info(sys_call_number);
+					getchar();
+				}
+				else if (TRACER_FLAG_2) {
+					print_sys_call_info(sys_call_number);
+				}
+        sys_call_seen(sys_call_number);
+      }
+      else{
+        sys_call_called = FALSE;
+      }
+      ptrace(PTRACE_SYSCALL, child_pid, NULL, NULL);
+    }
+
+		printf("\n-------FINAL SYS_CALLS TABLE--------\n");
+	  print_sys_calls_array();
+  }
+
+	return 0;
+}
+
+void manage_arguments(char* argv[]){
+	printf("\nThe first argument is: %s\n", argv[1]);
+	manage_V_argument(argv);
+	PROG_ARGS[PROG_NAME_POSITION] = PROG;
+	take_prog_arguments_from_argv(argv);
+	set_null_pointer_at_end(PROG_ARGS);
+}
+
+void take_prog_arguments_from_argv(char* argv[]){
+	if (V_flag_found()) {
+		if (prog_at_position_has_arguments(2)) {
+			copy_args_starting_from(PROG_ARGS, argv, 3);
+		}
+	}
+	else{
+		if (prog_at_position_has_arguments(1)) {
+			copy_args_starting_from(PROG_ARGS, argv, 2);
+		}
+	}
+}
+
+void manage_V_argument(char* argv[]){
 	if (!strcmp(argv[1], TRACER_OPTION_1)) { // -V flag found
 		printf("\nHas -V\n");
 		TRACER_FLAG_1 = TRUE;
@@ -48,33 +133,6 @@ int main(int argc, char *argv[]){
 		printf("\nHas no -V or -v flag\n");
 		NO_TRACER_FLAG = TRUE;
 		PROG = argv[1]; // Save PROG name
-
-	}
-
-	PROG_ARGS[0] = PROG;
-	take_prog_arguments_from_argv(argv);
-	set_null_pointer_at_end(PROG_ARGS);
-
-	printf("\nEl programa a ejecutar es %s\n", PROG);
-	printf("\nLa lista de argumentos de %s son:\n", PROG);
-	print_arguments(PROG_ARGS);
-	printf("-------EJECUCION DEL OTRO PROGRAMA--------\n");
-
-	execvp(PROG, PROG_ARGS);
-
-	return 0;
-}
-
-void take_prog_arguments_from_argv(char* argv[]){
-	if (V_flag_found()) {
-		if (prog_at_position_has_arguments(2)) {
-			copy_args_starting_from(PROG_ARGS, argv, 3);
-		}
-	}
-	else{
-		if (prog_at_position_has_arguments(1)) {
-			copy_args_starting_from(PROG_ARGS, argv, 2);
-		}
 	}
 }
 
